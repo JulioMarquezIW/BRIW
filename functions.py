889f93cd1fb270274f5a35440000d0b0233a00ce
Julio Marquez <julio.marquez@infinityworks.com>
2019-09-09 11:54:56 +0100

    Fix 3 and 4 options

--- functions.py
+++ functions.py
@@ -32,7 +32,7 @@ def ask_input_int(text):
 
 
 def add_drink():
-    drink = ask_input_string()
+    drink = ask_input_string('Enter the drink name please: ')
     data.drinks.append(drink)
 
 
@@ -41,27 +41,28 @@ def create_new_person():
     print(texts.NEW_PERSON)
     name = ask_input_string(texts.ENTER_NAME)
     drink = ask_input_string(texts.ENTER_FAVOURITE_DRINK)
-    return Person(name, drink)
+    data.people.append(Person(name, drink))
 
 
 def get_option():
+    op = None
     try:
         op = int(input("Enter the option: "))
-        if op == 1:
-            printer_aux.print_list(texts.DRINKS, data.drinks)
-        elif op == 2:
-            printer_aux.print_users()
-        elif op == 3:
-            add_drink()
-        elif op == 4:
-            create_new_person()
-        elif op == 0:
-            exit()
-        else:
-            print("Incorrect option, please retry:")
-            get_option()
     except ValueError:
         print("Please entry an integer.")
+    if op == 1:
+        printer_aux.print_list(texts.DRINKS, data.drinks)
+    elif op == 2:
+        printer_aux.print_users()
+    elif op == 3:
+        add_drink()
+    elif op == 4:
+        create_new_person()
+    elif op == 0:
+        exit()
+    else:
+        print("Incorrect option, please retry:")
+        get_option()
 
 
 def args_options(arg):
--- printer_aux.py
+++ printer_aux.py
@@ -1,5 +1,3 @@
-
-
 from os import system
 import texts
 import data
