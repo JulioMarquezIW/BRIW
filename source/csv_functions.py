6cd8254bf258a5c335c8afc178cee79e3cfd2e5a
Julio Marquez <julio.marquez@infinityworks.com>
2019-09-11 11:35:10 +0100

    Add csv file lines

--- source/csv_functions.py
+++ source/csv_functions.py
@@ -1,30 +1,21 @@
 
 def save_to_csv(people, drinks, filepath):
 
-    # Lock for the bigest list
-    biggest = []
-    smallest = []
-    if len(drinks) >= len(people):
-        biggest = drinks
-        smallest = people
-    else:
-        smallest = drinks
-        biggest = people
+    lines = []
+    titles = ["People", "Favourite Drinks", "Available Drinks"]
 
-    try:
-        with open(filepath, "w") as drinks_file:
-            drinks_file.write("Drinks\n")
-            for drink in drinks:
-                drinks_file.write(f"{drink}\n")
-    except FileNotFoundError as filenotfound:
-        print(
-            f"Could no open the file {filepath}. /nError: {str(filenotfound)}")
-    except Exception as e:
-        print(
-            f"Error opening the file {filepath}. /nError: {str(e)}")
+    # try:
+    #     with open(filepath, "w") as csv_file:
+
+    # except FileNotFoundError as filenotfound:
+    #     print(
+    #         f"Could no open the file {filepath}. /nError: {str(filenotfound)}")
+    # except Exception as e:
+    #     print(
+    #         f"Error opening the file {filepath}. /nError: {str(e)}")
 
 
 people = ['Adrian', 'Alba', 'Julio']
 drinks = ['water', 'cocacola', 'coffee']
-filepath = "dataWithoutLibray.csv"
+filepath = "data/data.csv"
 save_to_csv(people, drinks, filepath)
