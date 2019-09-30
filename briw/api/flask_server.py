5eec057e25a7c95361e316b4e95638a2ef6312f3
Julio Marquez <julio.marquez@infinityworks.com>
2019-09-30 11:27:17 +0100

    Add basic flask form example

--- briw/api/flask_server.py
+++ briw/api/flask_server.py
@@ -1,5 +1,8 @@
 from flask import Flask
 import os
+from flask import request
+from flask import render_template
+from briw.persistence.drinks_controller import get_drinks_from_database, save_new_drink_in_database
 
 
 template_dir = os.path.dirname(os.path.dirname(
@@ -9,12 +12,36 @@ template_dir = os.path.join(template_dir, 'html')
 print(template_dir)
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=template_dir)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/drinks', methods=['GET', 'POST'])
+def drinks():
+    if request.method == 'GET':
+        drinks = get_drinks_from_database()
+        return render_template('drinks.html', title='Drinks', drinks=drinks)
+
+    else:
+        return "Unsupported HTTP Request Type"
+
+
+@app.route('/form-example', methods=['GET', 'POST'])
+def form_example():
+    if request.method == 'POST':
+        firstname = request.form.get('firstname')
+        lastname = request.form['lastname']
+        return render_template('myPostedPage.html', title='Posted', firstname=firstname, lastname=lastname)
+
+    if request.method == 'GET':
+        return render_template('myFormPage.html', title='Post')
+
+    else:
+        return "Unsupported HTTP Request Type"
 
 
 if __name__ == '__main__':
--- script.sql
+++ script.sql
@@ -1,6 +1,8 @@
 
 USE julio;
 
+DROP TABLE BrewOrder;
+DROP TABLE BrewRound;
 DROP TABLE Person;
 DROP TABLE Drink;
 
@@ -17,11 +19,43 @@ CREATE TABLE Person
     person_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR (100),
     favourite_drink_id INTEGER,
-    FOREIGN KEY (favorite_drink_id) REFERENCES Drink(drink_id)
+    FOREIGN KEY (favourite_drink_id) REFERENCES Drink(drink_id)
 ); 
+
+
+CREATE TABLE BrewRound
+(
+    round_id INTEGER AUTO_INCREMENT PRIMARY KEY, 
+    is_open BOOLEAN, 
+    brewer INTEGER,
+    open_date DATETIME,
+   	FOREIGN KEY (brewer) REFERENCES Person(person_id)
+); 
+
+CREATE TABLE BrewOrder
+(
+    order_id INTEGER AUTO_INCREMENT PRIMARY KEY, 
+    drink_id INTEGER,
+    person_id INTEGER,
+    round_id INTEGER,
+    FOREIGN KEY (drink_id) REFERENCES Drink(drink_id),
+    FOREIGN KEY (person_id) REFERENCES Person(person_id),
+    FOREIGN KEY (round_id) REFERENCES BrewRound(round_id)
+); 
+
 
 
 INSERT INTO Drink(name) VALUES ('Water'),('Coffee'),('Tea');
 
 
-INSERT INTO julio.Person(name,favorite_drink_id) VALUES ('Eduardo', 1), ('Julio', 1), ('Henry', 3);
+INSERT INTO julio.Person(name,favourite_drink_id) VALUES ('Eduardo', 1), ('Julio', 1), ('Henry', 3);
+
+INSERT INTO BrewRound(is_open,brewer,open_date) VALUES (FALSE, 1, CURRENT_TIMESTAMP), (FALSE, 2, CURRENT_TIMESTAMP), (TRUE, 2, CURRENT_TIMESTAMP);
+
+INSERT INTO BrewOrder(drink_id, person_id, round_id) VALUES (1,2,1), (2,1,1), (3,1,2), (1,3,2), (1,2,2), (1,2,3);
+
+
+
+
+
+
